c3111f60fc0b43acb1e1cb313e7120cc85495fa5
mururu <yuki@gnnk.net>
2011-10-15 22:30:57 +0900

    added 'define' for using valiables

--- scheme.rb
+++ scheme.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+$variables = Hash.new
+
 def tokenize(line)
   temp = line.split()
   tokens = Array.new
@@ -25,23 +27,37 @@ def parse(tokens)
     inner_contents = contents.slice!(first..last)
     contents[first,0] = parse(inner_contents)
   end
-  p contents
+  if contents[0] == "define"
+    unless $variables.has_key?(contents[1].intern)
+      $variables[contents[1].intern] = eval(contents[2])
+    end
+  end
+  if contents[0] == "set!"
+    if $variables.has_key?(contents[1].intern)
+      $variables[contents[1].intern] = eval(contents[2])
+    end
+  end
   if contents[0] == "+"
-    return contents[1...(contents.length)].inject(0){|sum, i| sum + i.to_i }
+    return contents[1...(contents.length)].inject(0){|sum, i| sum + parse([i]) }
   end
   if contents[0] == "-"
-    return 2 * contents[1].to_i - contents[1...(contents.length)].inject(0){|sum, i| sum + i.to_i }
+    return 2 * parse([contents[i]]) - contents[1...(contents.length)].inject(0){|sum, i| sum + parse([i]) }
   end
   if contents[0] == "*"
-    return contents[1...(contents.length)].inject(1){|sum, i| sum * i.to_i }
+    return contents[1...(contents.length)].inject(1){|sum, i| sum * parse([i]) }
   end
   if contents[0] == "/"
-    return contents[1].to_i * contents[1].to_i / contents[1...(contents.length)].inject(1){|sum, i| sum * i.to_i }
+    return parse([contents[1]]) * parse([contents[1]]) / contents[1...(contents.length)].inject(1){|sum, i| sum * parse([i]) }
+  end
+  if tokens[0] =~ /[1-9][0-9]*/ or tokens[0] == "0" or tokens[0] =~ /[1-9][0-9]*\.[0-9]+/ or tokens =~ /0\.[0-9]+/
+    return tokens[0].to_f
+  else
+    return $variables[tokens[0].intern]
   end
 end
 
 
 while(print "> "; line = gets)
-  p parse(tokenize(line))
+  out = parse(tokenize(line))
+  p out unless out.nil?
 end
-
